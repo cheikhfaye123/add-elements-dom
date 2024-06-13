039e9a160fa7423eef955b18806b6ef3897f595d
cheikhfaye123 <baboudieye58@gmail.com>
2024-06-13 21:09:10 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+ const agregarBtn= document.getElementById('agregar');
+ const lista = document.getElementById('lista');
+ agregarBtn.addEventListener('click'
+ ,function(){
+    const nuevoElemento = prompt('ingresa el nuevo:');
+    if(nuevoElemento){
+        const li = document.createElement('li');
+        li.textContent = nuevoElemento;
+        lista.appendChild(li);
+    }
+ });
